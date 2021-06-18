4aa64e491f0b941541669983746fd25036bd00fe
emmedam <emmedam@gmail.com>
2021-06-18 23:03:41 +0200

    modificata grafica post e iniziato a lavorare su edit e show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,22 @@
 class PostsController < ApplicationController
+
     def index
         @posts = Post.all
     end
+    
+    def show
+    end
 
+    def edit 
+    end
+
+    
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    
+    def post_params
+      params.require(:post).permit(:title, :description, :image, :tag, :position)
+    end
 end
