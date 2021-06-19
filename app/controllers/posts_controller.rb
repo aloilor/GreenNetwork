36c421a3915dea23cd693db860592ef3e41213ea
emmedam <emmedam@gmail.com>
2021-06-19 07:37:59 +0200

    modificata grafica post e iniziato a lavorare su edit e show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,26 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
-    
+
     def show
+        @post = Post.find(params[:id])
     end
 
     def edit 
+        @post = Post.find(params[:id])
     end
 
+    def update 
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+          redirect_to @post
+        else
+          render :edit
+        end
     
-    def set_post
-      @post = Post.find(params[:id])
-    end
+    end 
 
-    
     def post_params
-      params.require(:post).permit(:title, :description, :image, :tag, :position)
+        params.require(:post).permit(:title, :description, :image, :tag, :position)
     end
 end
