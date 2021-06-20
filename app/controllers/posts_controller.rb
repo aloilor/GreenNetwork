c7f9f52f8be0c87b89c75347c3ded4bc4ca2bd00
Andrea99-git <andreadito99@gmail.com>
2021-06-20 17:45:50 +0200

    Added new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,20 @@ class PostsController < ApplicationController
         @posts = Post.all.order("created_at DESC")
     end
 
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
     def show
         @post = Post.find(params[:id])
     end
