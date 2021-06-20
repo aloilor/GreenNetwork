2c07ea9f5a2b2dade64061adc3f664624d6bdfc4
emmedam <emmedam@gmail.com>
2021-06-20 13:01:14 +0200

    modificata grafica form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,8 @@ class PostsController < ApplicationController
     
     end 
 
+    private 
+    
     def post_params
         params.require(:post).permit(:title, :description, :image, :tag, :position)
     end
