ab798b963c2ed369367838592afa7996e527a461
aloilor <aloisi.lorenzo99@gmail.com>
2021-07-08 12:37:35 +0200

    fixata roba

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_211651) do
+ActiveRecord::Schema.define(version: 2021_07_08_102943) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 2021_07_07_211651) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comments"
+    t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "post_id", null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
